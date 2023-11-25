144c87daf99a62dad8097b717350b6a31cd7eb9a
Sandeep <sndpgauti@gmail.com>
2023-11-25 22:55:53 +0530

    Added data ingestion

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
     
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -26,7 +26,7 @@ if __name__=="__main__":
         a=1/10
     except Exception as e:
         logging.info("Divide by Zero")
-        raise CustomeException(e,sys)
+        raise CustomException(e,sys)
 
 
     
