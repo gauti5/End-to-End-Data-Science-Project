2206932bed2d76e27e032de4c14528124472c6e9
Sandeep <sndpgauti@gmail.com>
2023-11-26 00:17:19 +0530

    Added Data Ingestion and artifacts

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,18 +1,22 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
+from exception import CustomException
+from logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclasses
+from dataclasses import dataclass
 
 
 
-@dataclasses
+@dataclass
 class DataIngestionConfig:
-    train_data_path:str = os.path.join('artifacts',"train.csv")
-    test_data_path:str = os.path.join('artifacts',"test.csv")
-    raw_data_path:str = os.path.join('artifacts',"data.csv")
+    train_data_path: str = os.path.join('artifacts',"train.csv")
+    test_data_path: str = os.path.join('artifacts',"test.csv")
+    raw_data_path: str = os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -21,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook/Data/stud.csv')
             logging.info('read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -32,8 +36,8 @@ class DataIngestion:
 
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set.to_csv(self.ingestion_config,train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config,test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
 
@@ -42,7 +46,7 @@ class DataIngestion:
                 self.ingestion_config.test_data_path,
             )
         except Exception as e:
-            raise CustomeException(e,sys)
+            raise CustomException(e,sys)
         
 
 if __name__=="__main__":
